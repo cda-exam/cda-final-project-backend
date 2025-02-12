480b8ed7dc20bbf4c6c3c0c3c28d2ca8b1872f17
timothedepoorter2 <depoortertimothe2@gmail.com>
2025-02-12 11:38:33 +0100

    chore: test with high severity issue

--- src/main/java/fr/cda/cdafinalprojectbackend/ProductService.java
+++ src/main/java/fr/cda/cdafinalprojectbackend/ProductService.java
@@ -1,8 +1,22 @@
 package fr.cda.cdafinalprojectbackend;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ProductService {
     String _namme;
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    public void unsafeQuery(String userInput) {
+        String query = "SELECT * FROM users WHERE username = '" + userInput + "'"; // SQL Injection possible
+        jdbcTemplate.execute(query);
+    }
+    public void testBug() {
+        String str = null;
+        System.out.println(str.length()); // NullPointerException potentiel (bug critique)
+    }
 }
